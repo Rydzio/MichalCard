c3a4c987f51326fb6c193ecc5431b011055b548e
rytych_ <210309@edu.p.lodz.pl>
2021-02-06 14:40:21 +0100

    Initial Commit

---
+++ MichalCard/MichalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MichalCardApp.swift
+//  MichalCard
+//
+//  Created by Michał Rytych on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MichalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
